9d0c62f89c74e4c1d9e9865a9c094974be1333ac
nikhil <nik70655@gmail.com>
2025-08-04 20:39:50 +0530

    vercel connected

--- api/server.js
+++ api/server.js
@@ -9,7 +9,9 @@ dotenv.config();
 const app = express()
 
 connectDB();
-app.use(cors())
+app.use(cors({
+  origin: 'https://mern-frontend-eight-kappa.vercel.app/'
+}))
 app.use(express.json())
 
 app.use('/api/tasks', taskRoutes);
