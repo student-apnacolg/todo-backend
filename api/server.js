4738d7a9e2926ba2f8fb4d6572ce67ed3115aec1
nikhil <nik70655@gmail.com>
2025-08-04 20:27:23 +0530

    2

---
+++ api/server.js
@@ -0,0 +1,21 @@
+const serverlessExpress = require('serverless-http');
+const express = require('express')
+const cors = require('cors')
+const dotenv = require('dotenv')
+const connectDB = require('../src/config/db');
+const taskRoutes = require('../src/routes/taskRoutes');
+
+dotenv.config();
+const app = express()
+
+connectDB();
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/tasks', taskRoutes);
+
+app.get('/', (req,res) => {
+  res.send('API running...')
+})
+
+module.exports = serverlessExpress(app)
