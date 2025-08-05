072d7de913e0944b1690f2dba96f9a8c2fa4a72c
nikhil <nik70655@gmail.com>
2025-08-05 10:59:01 +0530

    26 added render and removed vercel

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const cors = require('cors')
+const dotenv = require('dotenv')
+const connectDB = require('./src/config/db');
+const taskRoutes = require('./src/routes/taskRoutes');
+
+dotenv.config();
+
+const app = express()
+
+connectDB();
+
+app.use(cors({
+  origin: 'https://todo-frontend-steel-eight.vercel.app/',
+  credentials: true
+}))
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.status(200).send('✅ API is running');
+});
+
+app.use('/api/tasks', taskRoutes);
+
+app.listen(5000, () => {
+  console.log('server is up and Running at http://localhost:5000')
+})
